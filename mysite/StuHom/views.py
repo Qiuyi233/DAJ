029097c3c35c7525820ca7dcdbf549b2be48e065
daj <1737514099@qq.com>
2020-12-02 17:32:58 +0800

    StuHom的再次完善

--- mysite/StuHom/views.py
+++ mysite/StuHom/views.py
@@ -6,3 +6,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the StuHom index.")
+
+from .models import Student, Homework
+
+from django.views.generic.edit import CreateView
+
+class HomeworkCreate(CreateView):
+    model = Homework
+    template_name = 'homework_form.html'
+    fields = ['commit_date','headline','attach','remark', 'student']
